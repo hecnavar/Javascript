edce5cf2b8a9f7989268f853d9e748fa0eb0e33c
hecnavar <hmnm0007@gmail.com>
2025-01-27 20:48:10 -0600

    Fix: New files to practice with Js

---
+++ Session_1/Introduction.js
@@ -0,0 +1,44 @@
+const saludo = "Hola Mundo";
+
+const perro = "Firulais";
+
+const edad = 20;
+
+//Ejemplo de Camel Case
+
+const immpuestosAnuales = 25000;
+
+console.log(saludo)
+console.log(perro)
+console.log(edad)
+console.log(immpuestosAnuales)
+console.log()
+
+typeof perro
+console.log(typeof edad)
+console.log(typeof perro)
+//EJEMPLOS BEDUUU-----------------
+// Numbers
+typeof 10;	// "number"
+typeof 12.5;	// "number"
+typeof 2.5e+6;	// "number"
+typeof Infinity;	// "number"
+typeof NaN;	// "number"
+
+// Strings
+typeof ' ';	// "string" - Espacios en blanco cuentan como caracteres
+typeof 'Hello World';	// "string"
+typeof '12';	// "string" - Números dentro de comillas son strings
+
+// Booleans
+typeof true;	// "boolean"
+typeof false;	// "boolean"
+
+// Undefined
+typeof undefined;	// "undefined"
+
+var name;
+typeof name; // "undefined" - Aún no se asigna un valor
+
+// Null
+typeof Null;	// "object"
