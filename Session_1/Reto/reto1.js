3ceb9d90d6d5969ea9a5db431c4242f215bc97ae
hecnavar <hmnm0007@gmail.com>
2025-01-29 21:08:35 -0600

    Feat: New file with reto solved

---
+++ Session_1/Reto/reto1.js
@@ -0,0 +1,22 @@
+var a = 5;
+var b = 10;
+var c = 15;
+
+x1 = (3 + b) * c / a * 2;
+
+x2 = ((a + b * c) / 5) * 2;
+
+x3 = b / a + 2 * c;
+
+x4 = (a + b + c / c) * a;
+
+x5 = ((3 * a) / c) + a + b + c;  
+
+x6 = a - (b + c) * a / 1;
+
+console.log('My answer is: 78 The answer is: ' + x1)
+console.log('My answer is: 72 The answer is: ' + x2)
+console.log('My answer is: 32 The answer is: ' + x3)
+console.log('My answer is: 90 The answer is: ' + x4)
+console.log('My answer is: 31 The answer is: ' + x5)
+console.log('My answer is: 120 The answer is: ' + x6)
